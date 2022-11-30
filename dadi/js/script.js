62fa8a57f6535228f6ba1109033ceb55b78aaad3
Alessandro <alessandrogattuso@yahoo.it>
2022-11-30 18:45:54 +0100

    added dadi comments and readme file

--- dadi/js/script.js
+++ dadi/js/script.js
@@ -1,6 +1,10 @@
+// Create the 2 numbers randomly(one for the user and the other for the AI)
 let aiNumber = Math.floor(Math.random() * (7 - 1) + 1); // in range 1 to 6 =>  Math.floor(Math.random() * (max - min)) + min)
 let userNumber = Math.floor(Math.random() * (7 - 1) + 1); 
 
+//If user n > AI n => user win, 
+//If user n < AI n => AI win,
+//Else user n == A n => draw
 if(aiNumber > userNumber)
     console.log(`Tu hai lanciato ${userNumber} e L'IA ha lanciato ${aiNumber}, quindi hai perso`);
 
---
+++ dadi/readme.txt
@@ -0,0 +1,7 @@
+1 - Create 2 numbers randomly in a range(1,6) and save it in 2 variables(one for the user and the other for the AI)
+2 - IF user number > AI number 
+        - user win
+3 - IF ELSE user number < AI number
+        - AI win
+4 - ELSE 
+        - Draw
