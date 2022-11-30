cecf77f330d20e6e375992ee858e622fee0c70c6
Alessandro <alessandrogattuso@yahoo.it>
2022-11-30 18:39:59 +0100

    added mail readme and comments

--- mail/js/script.js
+++ mail/js/script.js
@@ -1,16 +1,22 @@
-let btn = document.querySelector("button");
+// Array of emails
 const listEmail = ["alex@gmail.com", "meeseeks2015@outlook.it", "techcompany@gmail.com"];
+// btn from html page
+let btn = document.querySelector("button");     
+// Flag that will allow us to say if the email is in the array
 let flag;
 
-flag = false;
+flag = false;  
 
 btn.addEventListener("click", function(){
+  
+  //Scroll through the array and search if the user's input exist in the array
   for(let i = 0; i < listEmail.length; i++){
     if(document.querySelector("input").value == listEmail[i]){
-        flag = true;
+        flag = true;    //if input's user is inside the array we set flag to true
         break;
     }
   }
+
   if(flag){
     document.querySelector("#answer").classList += " text-success";
     document.querySelector("#answer").innerText = "Email verificata";
@@ -20,5 +26,5 @@ btn.addEventListener("click", function(){
     document.querySelector("#answer").innerText = "L'email che hai inserito non esiste";
   }
 
-  flag = false;
+  flag = false; //At the end we set flag = false because if the user click the btn again this flag must not be true
 })
---
+++ mail/readme.txt
@@ -0,0 +1,11 @@
+1 - Create the array
+2 - Take the button from html with a query selector
+3 - Create a variable called flag to use it to know if the email's user is inside the array
+4 - Scroll the array
+    4.1 IF the input's element is equal to an element of the array
+          - Set flag to true
+          - Break the cicle 
+5 - IF flag is true
+        - Print that the email is valid
+    ELSE 
+        - Print that the email doesn't exist
