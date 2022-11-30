7624ac2e2ab7e3705081bffbb1f71c9d20237334
Alessandro <alessandrogattuso@yahoo.it>
2022-11-30 21:40:07 +0100

    comments about the condition are now in order

--- dadi/js/script.js
+++ dadi/js/script.js
@@ -2,14 +2,14 @@
 let aiNumber = Math.floor(Math.random() * (7 - 1) + 1); // in range 1 to 6 =>  Math.floor(Math.random() * (max - min)) + min)
 let userNumber = Math.floor(Math.random() * (7 - 1) + 1);  // with max exluded(this is why i put 7 as max and not 6)
 
-//If user n > AI n => user win, 
-//If user n < AI n => AI win,
-//Else user n == A n => draw
+//If user n > AI n => user win
 if(aiNumber > userNumber)
     console.log(`Tu hai lanciato ${userNumber} e L'IA ha lanciato ${aiNumber}, quindi hai perso`);
 
+//If user n < AI n => AI win
 else if(aiNumber < userNumber)
 console.log(`Tu hai lanciato ${userNumber} e L'IA ha lanciato ${aiNumber}, quindi hai vinto`);
 
+//Else user n == A n => draw
 else
     console.log(`Entrambi avete lanciato ${userNumber}, quindi è pareggio`);
